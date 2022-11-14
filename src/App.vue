8de9664a098f4e8eac67a2b1996d70c38a6b1d33
snerva <selenenerva@live.it>
2022-11-14 17:09:38 +0100

    print img first commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,60 @@
 <script>
+import axios from 'axios'
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      API_URL: 'https://www.breakingbadapi.com/api/characters',
+      characters: null,
+      info: null,
+      error: null
+    }
+  },
+  methods: {
+    callApi(url) {
+      axios.get(url)
+        .then(response => {
+          console.log(response);
+          this.characters = response.data;
+        })
+        .catch(err => {
+          console.error(err.message);
+          this.error = err.message
+        })
+    }
+  },
+  mounted() {
+    this.callApi(this.API_URL)
+  }
 }
 </script>
 
 <template>
-  <h1>hello</h1>
+  <header>
+    <h1 class="m-3">Breaking Bad Api</h1>
+  </header>
+
+  <main>
+    <div class="container">
+      <select class="form-select">
+        <option selected>Select category</option>
+      </select>
+
+      <div class="characters">
+        <div class="row row-cols-5">
+          <div class="col p-3" v-for="character in characters">
+            <div class="character">
+              <img :src="character.img" :alt="character.name" class="img-fluid">
+              <h4>{{ character.name }}</h4>
+              <div class="category">{{ character.category }}</div>
+              <div class="status">{{ character.status }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
 </template>
 
 <style lang="scss" scoped>
