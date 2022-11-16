2241ee4cd33283b9ef2cdbbef2b8fe7bde00ebd3
snerva <selenenerva@live.it>
2022-11-16 09:27:49 +0100

    Bonus found characters number

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -11,6 +11,11 @@ export default {
         return {
             store
         }
+    },
+    computed: {
+        totalResults() {
+            return this.store.characters.length
+        }
     }
 }
 </script>
@@ -18,7 +23,7 @@ export default {
 <template>
     <div class="characters p-4 my-5">
         <div class="characters_numbers">
-            <p class="p-3">Found 62 characters</p>
+            <p class="p-3" v-if="store.characters !== null">Found {{ totalResults }} characters</p>
         </div>
         <div class="row row-cols-5 p-3  g-3 text-center">
             <CharacterItem :character="character" v-for="character in store.characters"></CharacterItem>
